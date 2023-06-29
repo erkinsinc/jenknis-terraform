94299d7dbc6bec7a3f86c91602b759fdde30c645
erkinsinc <69076596+erkinsinc@users.noreply.github.com>
2023-06-28 20:35:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-2"
 resource "aws_instance" "web" {
 ami = "ami-092b51d9008adea15"
 instance_type = "t2.micro"
-availabilty = "us-east-2a"
+availability_zone = "us-east-2a"
 key_name = "macbook-2023June"
 }
 
