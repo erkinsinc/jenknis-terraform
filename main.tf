eb824b6f1a7791d81763a8c9b4ddc08d9115288e
Schuma <erkinsinc@gmail.com>
2023-06-29 15:07:34 -0500

    added variables

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ region = "us-east-2"
 }
 
 resource "aws_instance" "web" {
-ami = "ami-092b51d9008adea15"
-instance_type = "t2.micro"
-availability_zone = "us-east-2a"
-key_name = "macbook-2023June"
+ami = var.ami_id
+instance_type = var.instance_type
+availability_zone = var.az1
+key_name = var.key_pair
 }
 
 
